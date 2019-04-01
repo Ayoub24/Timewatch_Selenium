62312bdcc181a1da24ac7ea1af7623edbf4bb17c
aayoub <ayoub.anwar24@gmail.com>
2019-04-02 00:37:21 +0200

    Get links from timewatch website

--- src/newpackage/MyClass.java
+++ src/newpackage/MyClass.java
@@ -1,9 +1,13 @@
 package newpackage;
+
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.PrintWriter;
+import org.json.JSONObject;
 
 public class MyClass
 {
@@ -13,16 +17,28 @@ public class MyClass
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Documents\\Eclipse_workspace\\chromedriver.exe");
 		// tries to open chercher.tech page
 		driver.get("http://timewatch.ma");
-		WebElement Marques = driver.findElement(By.xpath("//a[@href='http://timewatch.ma/categorie-produit/footwear/']//following-sibling::ul"));
-		//Marques.getAttribute("innerHTML");
-		//System.out.println(Marques.getAttribute("innerHTML"));
-		List<WebElement> MarqueList = Marques.findElements(By.xpath("//following-sibling::li"));
-		//System.out.println("hello "+ MarqueList);
-		for (WebElement we:MarqueList)
-        {
-			System.out.println("hello");
-			System.out.println(we.getAttribute("innerHTML"));
-        } 
+
+		List<WebElement> MarqueList = driver.findElements(By.xpath("//a[@href='http://timewatch.ma/categorie-produit/footwear/']//following-sibling::ul//following-sibling::li/a"));
+		
+
+	    //Create instance of JSONObject
+	    JSONObject jObj = new JSONObject();
+	      
+		for (int i=0; i<MarqueList.size();i++){
+		      System.out.println(MarqueList.get(i).getAttribute("textContent"));
+		      System.out.println(MarqueList.get(i).getAttribute("href"));
+		      
+		      
+		      //writing test data to the JSON by put method
+		      jObj.put(MarqueList.get(i).getAttribute("textContent"), MarqueList.get(i).getAttribute("href"));
+		      
+		    }
+		//Writing test data to the JSONWrite.json JSON file
+	    PrintWriter pWriter = new PrintWriter("files/MARQUES.json");
+	    pWriter.write(jObj.toString());
+	      
+	    pWriter.flush();
+	    pWriter.close();
 		driver.close();
 	}
 }
